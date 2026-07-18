6e1d5df8c2642a1cbd17cb12bcd3b638eda1160d
Asmita <asmitay2003@gmail.com>
2026-07-18 23:14:35 +0530

    Remove temporary seed route

--- server/index.js
+++ server/index.js
@@ -17,9 +17,6 @@ const wishlistRoutes = require("./routes/wishlistRoutes");
 
 const contactRoutes = require("./routes/contactRoutes");
 
-const Product = require("./models/Product");
-const products = require("./seed/products");
-
 connectDB();
 
 const app = express();
@@ -46,26 +43,6 @@ app.get("/", (req, res) => {
 
   res.send("🚀 FurniHome Backend Running Successfully");
 
-});
-
-app.get("/api/seed", async (req, res) => {
-  try {
-    await Product.deleteMany();
-
-    await Product.insertMany(products);
-
-    res.json({
-      success: true,
-      message: "Products seeded successfully",
-    });
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
 });
 
 app.listen(PORT, () => {
